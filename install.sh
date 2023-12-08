fc29b56f513ab3cd1d59edb3befa2676c7f65625
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2023-12-08 21:43:41 +0900

    updated

--- install.sh
+++ install.sh
@@ -5,14 +5,17 @@ ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 hwclock --systohc
 nano /etc/locale.gen # en_US, ja_JP
 locale-gen
-nano /etc/locale.conf # LANG=ja_JP.UTF-8
-nano /etc/vconsole.conf # KEYMAP=jp106
+nano /etc/locale.conf
+# > LANG=en_US.UTF-8
+nano /etc/vconsole.conf
+# > KEYMAP=jp106
+# > XKBLAYOUT=jp,us
 nano /etc/hostname
 nano /etc/pacman.conf
 nano /etc/pacman.d/mirrorlist
 sudo pacman -S networkmanager network-manager-applet lvm2
 nano /etc/mkinitcpio.conf
-mkinicpio -p linux
+mkinitcpio -p linux
 passwd
 sudo pacman -S amd-ucode
 bootctl install
@@ -24,17 +27,29 @@ pacman -S nvidia nvidia-settings
 nano /etc/mkinitcpio.conf # remove `kms` from HOOKS
 nano /boot/loader/entries/arch.conf
 
+nano /etc/fstab
+# > /dev/mapper/swap none swap sw 0 0
+nano /etc/crypttab
+# > swap /dev/mapper/arch-swap /dev/urandom swap,cipher=aes-xts-plain64,size=512
+mkinitcpio -p linux
+
 git clone https://github.com/KeishiS/dotfiles.git
 sudo pacman -S - < dotfiles/pacman.list
 install paru
 paru -S - < dotfiles/aur.list
 
+nano /etc/vconsole.conf
+# > FONT=spleen-16x32
+nano /etc/ykfde.conf
+
 #---
 nano /etc/sddm.conf
-> [Theme]
-> Current=sugar-dark
+# > [Theme]
+# > Current=sugar-dark
 localectl set-x11-keymap jp,us
 systemctl enable NetworkManager
 systemctl start NetworkManager
+systemctl enable sddm
+timedatectl set-ntp true
 
 systemctl enable pcscd
