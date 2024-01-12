4d82ec40de1204adf2263b97c2f2d3e70534d45a
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2024-01-12 10:03:13 +0900

    updated

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
-sudo pacstrap -K /mnt base base-devel linux linux-firmware nano git
+# setfont
+sudo pacman -S git fakeroot archlinux-keyring
+useradd -g wheel -m arch
+EDITOR=nano visudo
+su arch
+> cd
+> git clone https://aur.archlinux.org/spleen-font.git
+> cd spleen-font
+> makepkg
+> sudo pacman -U spleen-font_xxxxx
+
+sudo pacstrap -K /mnt base base-devel linux linux-firmware nano git zsh
 genfstab -U /mnt > /mnt/etc/fstab
 arch-chroot /mnt
 ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
@@ -13,7 +24,7 @@ nano /etc/vconsole.conf
 nano /etc/hostname
 nano /etc/pacman.conf
 nano /etc/pacman.d/mirrorlist
-sudo pacman -S networkmanager network-manager-applet lvm2
+sudo pacman -S networkmanager network-manager-applet lvm2 yubikey-full-disk-encryption
 nano /etc/mkinitcpio.conf
 mkinitcpio -p linux
 passwd
@@ -30,7 +41,7 @@ nano /boot/loader/entries/arch.conf
 nano /etc/fstab
 # > /dev/mapper/swap none swap sw 0 0
 nano /etc/crypttab
-# > swap /dev/mapper/arch-swap /dev/urandom swap,cipher=aes-xts-plain64,size=512
+# > swap /dev/mapper/arch-swap /dev/urandom swap,cipher=aes-xts-plain64,size=256
 mkinitcpio -p linux
 
 echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
