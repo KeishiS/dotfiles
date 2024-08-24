fa37a9d38f567e56b7455e0ac45f26253ace5f98
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2024-08-25 01:33:15 +0900

    updated

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,14 +5,15 @@ local config = {}
 if wezterm.config_builder then
     config = wezterm.config_builder()
 end
-config.color_scheme = 'MaterialDesignColors'
-config.window_background_opacity = 0.95
+--[[ config.color_scheme = 'MaterialDesignColors' ]]
+config.color_scheme = 'Tokyo Night'
+config.window_background_opacity = 0.8
 config.use_ime = true
 config.default_cursor_style = 'SteadyBar'
-config.font_size = 16.0
+config.font_size = 14.0
 config.font = wezterm.font_with_fallback {
     {
-        family = "Source Han Code JP"
+        family = "JetBrains Mono"
     },
     {
         family = "Fira Code",
