3b1f87a87e051ee50fb7ef622991aa02841a27e5
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2024-10-09 18:39:02 +0900

    updated

--- NixOS/flake.nix
+++ NixOS/flake.nix
@@ -3,15 +3,23 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    ragenix.url = "github:yaxitech/ragenix";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    my-secrets = {
+      url = "github:KeishiS/my-secrets/main";
+      flake = false;
     };
   };
 
   outputs = { nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.NixOS-keishis-X13 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = inputs;
       modules = [
         ./default.nix
         ./sway.nix
