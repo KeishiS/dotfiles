f6c66d382bbf77983f4a7db89e4894a4327b89cf
SANDO Keishi <50182133+KeishiS@users.noreply.github.com>
2024-10-19 00:40:26 +0900

    OBSのための設定追加 (#7)

--- NixOS/X13/hardware-configuration.nix
+++ NixOS/X13/hardware-configuration.nix
@@ -40,8 +40,13 @@
       };
     };
 
-    kernelModules = [ "kvm-amd" ];
-    extraModulePackages = [];
+    kernelModules = [ "kvm-amd" "v4l2loopback" ];
+    extraModulePackages = with config.boot.kernelPackages; [
+      v4l2loopback
+    ];
+    extraModprobeConfig = ''
+      options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+    '';
   };
 
   services.xserver = {
--- NixOS/home-srv/hardware-configuration.nix
+++ NixOS/home-srv/hardware-configuration.nix
@@ -9,8 +9,13 @@
   ];
 
   boot = {
-    kernelModules = [ "kvm-amd" ];
-    extraModulePackages = [];
+    kernelModules = [ "kvm-amd" "v4l2loopback" ];
+    extraModulePackages = with config.boot.kernelPackages; [
+      v4l2loopback
+    ];
+    extraModprobeConfig = ''
+      options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+    '';
 
     initrd = {
       availableKernelModules = [
