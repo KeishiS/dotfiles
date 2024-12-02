da3cd2c8a5a2db6ef7d008ed2ec1470d1089c109
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2024-12-03 05:45:32 +0900

    modified cursor-style of wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ config.front_end="WebGpu" -- workaround
 config.color_scheme = 'Tokyo Night'
 config.window_background_opacity = 0.8
 config.use_ime = true
--- config.default_cursor_style = 'SteadyBar'
+config.default_cursor_style = 'SteadyBar'
 config.font_size = 14.0
 config.font = wezterm.font_with_fallback {
   {
