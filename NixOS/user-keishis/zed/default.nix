946dac67fff8c12d102ac3d5c9d6703bcbf79275
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2025-03-04 17:40:52 +0900

    updated a setting for zed

--- NixOS/user-keishis/zed/default.nix
+++ NixOS/user-keishis/zed/default.nix
@@ -7,7 +7,10 @@
       buffer_font_size = 20;
       buffer_font_family = "JuliaMono";
       soft_wrap = "editor_width";
-      buffer_font_fallbacks = [ "JetBrainsMono Nerd Font" "Noto Sans Mono CJK JP" ];
+      buffer_font_fallbacks = [
+        "JetBrainsMono Nerd Font"
+        "Noto Sans Mono CJK JP"
+      ];
       vim_mode = true;
       theme = {
         mode = "system";
@@ -19,6 +22,16 @@
         show_type_hints = true;
         show_parameter_hints = true;
         show_other_hints = true;
+      };
+
+      languages = {
+        Nix = {
+          language_servers = [
+            "nil"
+            "!nixd"
+          ];
+          formatter.external.command = "nixfmt";
+        };
       };
     };
   };
