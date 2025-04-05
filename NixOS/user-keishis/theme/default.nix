e22e2fe340d6c4c162f08af880f29385ce0dca1f
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2025-04-05 12:24:59 +0900

    done hyprlock setting

--- NixOS/user-keishis/theme/default.nix
+++ NixOS/user-keishis/theme/default.nix
@@ -4,6 +4,8 @@ rec {
   bg2 = "#16161E";
   fg2 = "#565F89";
 
+  fg-rgb = "rgb(169, 177, 214)";
+  bg-rgb = "rgb(26, 27, 38)";
   bg-080 = "rgba(26, 27, 38, 0.8)";
   bg-090 = "rgba(26, 27, 38, 0.9)";
 
@@ -24,6 +26,8 @@ rec {
   success = green;
   error = red;
   highlight = yellow;
+
+  primary-rgb = "rgb(122, 162, 247)";
 
   console-font = "Monaspace Krypton";
 }
