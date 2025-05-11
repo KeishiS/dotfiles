c386b25f26378900dad1c9bf7374c2424d392971
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2025-05-12 03:32:25 +0900

    added netdata-client settings

--- NixOS/pkgs/netdata-client/default.nix
+++ NixOS/pkgs/netdata-client/default.nix
@@ -9,10 +9,40 @@
         "memory mode" = "ram";
         timezone = "Asia/Tokyo";
       };
+      plugins."go.d" = "yes";
     };
     configDir."stream.conf" = pkgs.writeText "stream.conf" ''
       [stream]
         enabled = yes
+        destination = nixos-sandi-lenovo
+        api key = 493e294a-1bcc-45c1-bdd3-e82b8dbc5ff9
     '';
+  };
+  systemd.services.netdata = {
+    path = [ "/run/wrappers" ];
+    serviceConfig.CapabilityBoundingSet = "CAP_SYS_RAWIO";
+  };
+
+  environment.etc."netdata/go.d.conf".text = ''
+    enabled: yes
+    default_run: yes
+    max_procs: 1
+    modules:
+      smartctl: yes
+  '';
+  environment.etc."netdata/go.d/smartctl.conf".text = ''
+    update_every: 60
+    scan_every: 600
+    poll_devices_every: 3600
+    jobs:
+      - name: smartctl
+  '';
+
+  environment.systemPackages = [ pkgs.smartmontools ];
+  security.wrappers.smartctl = {
+    source = "${pkgs.smartmontools}/bin/smartctl";
+    capabilities = "cap_dac_override,cap_sys_rawio+ep";
+    owner = "root";
+    group = "root";
   };
 }
