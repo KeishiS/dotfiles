f66bdc9b8cf784759ab36749d1cc95349c66ec6b
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2025-05-26 09:42:53 +0900

    updated for japanese input in 1password

--- NixOS/user-keishis/fcitx5/default.nix
+++ NixOS/user-keishis/fcitx5/default.nix
@@ -1,19 +1,17 @@
 { ... }:
 {
   i18n.inputMethod = {
-    /*
-      fcitx5.settings = {
-        inputMethod = {
-          GroupOrder."0" = "Default";
-          "Groups/0" = {
-            Name = "Default";
-            "Default Layout" = "jp106";
-            DefaultIM = "mozc";
-          };
-          "Groups/0/Items/0".Name = "mozc";
+    fcitx5.settings = {
+      inputMethod = {
+        GroupOrder."0" = "Default";
+        "Groups/0" = {
+          Name = "Default";
+          "Default Layout" = "jp106";
+          DefaultIM = "mozc";
         };
+        "Groups/0/Items/0".Name = "mozc";
       };
-    */
-    # fcitx5.waylandFrontend = false;
+    };
+    fcitx5.waylandFrontend = false;
   };
 }
