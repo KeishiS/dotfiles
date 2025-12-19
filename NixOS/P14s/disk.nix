d65a425500cae10fd012f3fa34e63d8787b58d0d
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2025-12-19 10:09:01 +0900

    added disko setting

---
+++ NixOS/P14s/disk.nix
@@ -0,0 +1,61 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "<rename-to-your-device>";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "lvm_pv";
+                vg = "pool";
+              };
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      pool = {
+        type = "lvm_vg";
+        lvs = {
+          swap = {
+            size = "16G";
+            content = {
+              type = "swap";
+              randomEncryption = true;
+            };
+          };
+          root = {
+            size = "100%FREE";
+            content = {
+              type = "luks";
+              name = "unlocked";
+              settings.allowDiscards = true;
+              passwordFile = "<path-to-temporary-password-file>";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+                mountOptions = [ "noatime" ];
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
