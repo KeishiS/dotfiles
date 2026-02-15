ca6511867b7f157e1703773e92e23b3c41641463
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2026-02-16 00:46:42 +0900

    updated

--- NixOS/lenovo/disk.nix
+++ NixOS/lenovo/disk.nix
@@ -76,5 +76,44 @@ in
         };
       };
     };
+
+    disk.main = {
+      type = "disk";
+      device = "<OS disk>";
+      content = {
+        type = "gpt";
+        partitions.ESP = {
+          size = "1G";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = [ "umask=0077" ];
+            extraArgs = [
+              "-n"
+              "BOOT"
+            ];
+          };
+        };
+        partitions.Swap = {
+          size = "16G";
+          type = "8200";
+          content = {
+            type = "swap";
+            randomEncryption = true;
+          };
+        };
+        partitions.root = {
+          size = "100%";
+          type = "8300";
+          content = {
+            type = "filesystem";
+            format = "ext4";
+            mountpoint = "/";
+          };
+        };
+      };
+    };
   };
 }
