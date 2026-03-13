5110af1123be8c2fb8dd8c1498ae0be407787726
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2026-03-13 20:55:57 +0900

    updated

--- NixOS/home-srv/nginx.nix
+++ NixOS/home-srv/nginx.nix
@@ -4,14 +4,45 @@
     enable = true;
 
     recommendedGzipSettings = true;
-    recommendedProxySettings = true;
+    # recommendedProxySettings = true;
 
-    upstreams.gql-servers = {
+    upstreams.graphql-gateways = {
       extraConfig = ''
         least_conn;
       '';
       servers = {
         "127.0.0.1:50001" = { };
+        "127.0.0.1:50002" = { };
+      };
+    };
+
+    upstreams.auth-services = {
+      extraConfig = ''
+        least_conn;
+      '';
+      servers = {
+        "127.0.0.1:51001" = { };
+        "127.0.0.1:51002" = { };
+      };
+    };
+
+    upstreams.user-query-services = {
+      extraConfig = ''
+        least_conn;
+      '';
+      servers = {
+        "127.0.0.1:52001" = { };
+        "127.0.0.1:52002" = { };
+      };
+    };
+
+    upstreams.user-command-services = {
+      extraConfig = ''
+        least_conn;
+      '';
+      servers = {
+        "127.0.0.1:53001" = { };
+        "127.0.0.1:53002" = { };
       };
     };
 
@@ -28,11 +59,73 @@
         }
       ];
       http2 = true;
-      http3 = true;
 
       locations."/" = {
-        proxyPass = "http://gql-servers";
+        proxyPass = "http://graphql-gateways";
         # root = "/var/www";
+      };
+    };
+
+    virtualHosts."keylytix-auth-service" = {
+      listen = [
+        {
+          addr = "127.0.0.1";
+          port = 51000;
+        }
+        {
+          addr = "[::1]";
+          port = 51000;
+        }
+      ];
+      extraConfig = ''
+        http2 on;
+      '';
+      locations."/" = {
+        extraConfig = ''
+          grpc_pass grpc://auth-services;
+        '';
+      };
+    };
+
+    virtualHosts."keylytix-user-query-service" = {
+      listen = [
+        {
+          addr = "127.0.0.1";
+          port = 52000;
+        }
+        {
+          addr = "[::1]";
+          port = 52000;
+        }
+      ];
+      extraConfig = ''
+        http2 on;
+      '';
+      locations."/" = {
+        extraConfig = ''
+          grpc_pass grpc://user-query-services;
+        '';
+      };
+    };
+
+    virtualHosts."keylytix-user-command-service" = {
+      listen = [
+        {
+          addr = "127.0.0.1";
+          port = 53000;
+        }
+        {
+          addr = "[::1]";
+          port = 53000;
+        }
+      ];
+      extraConfig = ''
+        http2 on;
+      '';
+      locations."/" = {
+        extraConfig = ''
+          grpc_pass grpc://user-command-services;
+        '';
       };
     };
   };
