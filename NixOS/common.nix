0889a274c66380c0765c7efd6e9bb0e19ddd43f7
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2026-03-16 16:11:04 +0900

    updated

--- NixOS/common.nix
+++ NixOS/common.nix
@@ -143,6 +143,7 @@
     initialHashedPassword = "$6$Rk3ZM8V5JpDmaggo$tADvEPoECdw7PE2JZebqch3rpsrDJAZ40JZt1aK6HpfZ9psXDy7I3XwCtoVCaMhFY8cJt.YVJuFQIExiwJgLs.";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICLPYWxCTckCVdDiBpiKWE8omDndrvQhWkscX8uIyd1j openpgp:0xD1E438FC"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMEyh4Y1n0kJy3zfPZm2sWilYOVf/nC0Ifvh7F95/1H6 openpgp:0xBE9282EA"
     ];
   };
 
