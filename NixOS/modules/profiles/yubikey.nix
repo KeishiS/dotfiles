3d8f23976795166f05d9d686d9eb75d41c58607a
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2026-07-27 04:46:49 +0900

    updated

--- NixOS/modules/profiles/yubikey.nix
+++ NixOS/modules/profiles/yubikey.nix
@@ -2,6 +2,11 @@
 {
   services.pcscd.enable = true;
 
+  environment.systemPackages = with pkgs; [
+    gnupg
+    pcsc-tools
+  ];
+
   # YubiKeyが抜かれた際に画面ロック
   services.udev.extraRules = ''
     ACTION=="remove", ENV{ID_VENDOR_ID}=="1050", ENV{ID_MODEL_ID}=="0407", RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
