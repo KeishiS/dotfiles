4357862ca6eef763648b12a18855bf12ce5d7053
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2026-08-13 03:19:19 +0900

    updated renkan

--- NixOS/hosts/lenovo/renkan/default.nix
+++ NixOS/hosts/lenovo/renkan/default.nix
@@ -9,6 +9,33 @@
     restartUnits = [ "renkan.service" ];
   };
 
+  sops.secrets.renkan-oidc-client-secret = {
+    format = "binary";
+    sopsFile = ./secrets/renkan-oidc-client-secret.enc;
+    owner = "root";
+    group = "root";
+    mode = "400";
+    restartUnits = [ "renkan.service" ];
+  };
+
+  sops.secrets.renkan-zotero-client-key = {
+    format = "yaml";
+    sopsFile = ./secrets/renkan-zotero-secret.enc.yaml;
+    owner = "root";
+    group = "root";
+    mode = "400";
+    restartUnits = [ "renkan.service" ];
+  };
+
+  sops.secrets.renkan-zotero-client-secret = {
+    format = "yaml";
+    sopsFile = ./secrets/renkan-zotero-secret.enc.yaml;
+    owner = "root";
+    group = "root";
+    mode = "400";
+    restartUnits = [ "renkan.service" ];
+  };
+
   services.renkan = {
     enable = true;
     baseUrl = "https://renkan.sandi05.com/";
@@ -16,6 +43,18 @@
     listenAddress = "0.0.0.0:6789";
     encryptionKeyFile = config.sops.secrets.renkan-encryption-key.path;
     syncIntervalSeconds = 600;
+
+    oidc = {
+      issuerUrl = "https://id.sandi05.com/oauth2/openid/renkan";
+      clientId = "renkan";
+      clientSecretFile = config.sops.secrets.renkan-oidc-client-secret.path;
+    };
+
+    zoteroOAuth = {
+      consumerKeyFile = config.sops.secrets.renkan-zotero-client-key.path;
+      consumerSecretFile = config.sops.secrets.renkan-zotero-client-secret.path;
+      allowGroupReadAccess = true;
+    };
   };
   networking.firewall.allowedTCPPorts = [ 6789 ];
 }
